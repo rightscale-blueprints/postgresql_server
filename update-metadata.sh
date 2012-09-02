2b50cfb0a2f75f656f0f74e2eb4861d6187cc5fa
Chris Fordham <chris.fordham@rightscale.com>
2012-09-02 17:59:42 +1000

    Use upstream opscode cookbook for postgresql; update all cookbooks via librarian-chef; re-generate metadata.json files.

--- update-metadata.sh
+++ update-metadata.sh
@@ -2,7 +2,6 @@
 
 # generate metadata.json for upstream opscode cookbooks that have no metadata.json
 knife cookbook metadata apache2 -o cookbooks/
-knife cookbook metadata build-essential -o cookbooks/
 knife cookbook metadata collectd -o cookbooks/
 knife cookbook metadata collectd_plugins -o cookbooks/
 knife cookbook metadata cron -o cookbooks/
@@ -10,6 +9,3 @@ knife cookbook metadata ntp -o cookbooks/
 knife cookbook metadata openssl -o cookbooks/
 knife cookbook metadata postfix -o cookbooks/
 knife cookbook metadata postgresql -o cookbooks/
-knife cookbook metadata python -o cookbooks/
-knife cookbook metadata sudo -o cookbooks/
-knife cookbook metadata yum -o cookbooks/
